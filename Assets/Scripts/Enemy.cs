7a649f29fb488338bac9a62a309fb04cd8b796fe
mrcompactxx <mayurbhagwat100@gmail.com>
2024-05-13 17:51:54 +0530

    Modified Enemy AI
    Earlier the enemy was stopping at 3f distance between them due to it's parent, that bug is resolved by adding the enemy gameobject to measure distance between them

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool isCollided;
+    public bool getIsCollided 
+    {
+        get { return isCollided; }
+    }
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Player") 
+        {
+            isCollided = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Player") 
+        {
+            isCollided=false;
+        }
+    }
+
+
 }
--- Assets/Scripts/EnemyHandler.cs
+++ Assets/Scripts/EnemyHandler.cs
@@ -6,22 +6,24 @@ public class EnemyHandler : MonoBehaviour
 {
     [SerializeField]private GameObject slimeEnemy;
     [SerializeField] private GameObject player;
+    private Enemy enemy;
     [SerializeField] private Animator enemyAnimator;
     [SerializeField]private float distance;
     [SerializeField] private float speed=10f;
     void Start()
     {
+        enemy = FindAnyObjectByType<Enemy>();
         slimeEnemy = this.gameObject.transform.GetChild(0).gameObject;       
         enemyAnimator = slimeEnemy.GetComponent<Animator>();
     }
 
     void Update()
     {
-        distance = Vector2.Distance(this.transform.position, player.transform.position);
-        if (distance<15f && distance>=0) 
+        distance = Vector2.Distance(slimeEnemy.transform.position, player.transform.position);
+        if (distance<15f) 
         {
-            transform.position = Vector2.MoveTowards(this.transform.position,player.transform.position,speed*Time.deltaTime);
-            if (distance <= 5f)
+            slimeEnemy.transform.position = Vector2.MoveTowards(slimeEnemy.transform.position,player.transform.position,speed*Time.deltaTime);
+            if (enemy.getIsCollided)
             {
                 enemyAnimator.SetBool("IsIdle", false);
                 enemyAnimator.SetBool("IsAttacking", true);
