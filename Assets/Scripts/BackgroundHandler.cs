0d617b6a5589eb278373912582bb361f1109b9a3
mrcompactxx <mayurbhagwat100@gmail.com>
2024-05-20 14:52:12 +0530

    Modified BackGround

--- Assets/Scripts/BackgroundHandler.cs
+++ Assets/Scripts/BackgroundHandler.cs
@@ -3,31 +3,33 @@ using UnityEngine;
 public class BackgroundHandler : MonoBehaviour
 {
     private float startingPos;
-    private float lenghtOfSprite;
+    private float lenghtOfSpriteX;
     [SerializeField]private float amountOfParallax;
     public Camera mainCamera;
     private void Start()
     {
         startingPos = transform.position.x;
-        lenghtOfSprite = GetComponent<SpriteRenderer>().bounds.size.x;
+        lenghtOfSpriteX = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     private void Update()
     {
         Vector3 position = mainCamera.transform.position;
-        float temp = position.x * (1 - amountOfParallax);
-        float distance = position.x * amountOfParallax;
+        float tempX = position.x * (1 - amountOfParallax);
+        float horizontalDistance = position.x * amountOfParallax;
 
-        Vector3 newPosition = new Vector3 (startingPos+distance, transform.position.y, transform.position.z);
+        Vector3 newPosition = new Vector3(startingPos + horizontalDistance, transform.position.y, transform.position.z);
         transform.position = newPosition;
 
-        if (temp > startingPos + lenghtOfSprite)
+        if (tempX > startingPos + lenghtOfSpriteX)
         {
-            startingPos += lenghtOfSprite;
+            startingPos += lenghtOfSpriteX;
         }
-        else if (temp<startingPos-lenghtOfSprite) 
+        else if (tempX < startingPos - lenghtOfSpriteX)
         {
-            startingPos -= lenghtOfSprite;
+            startingPos -= lenghtOfSpriteX;
         }
+   
+
     }
 }
